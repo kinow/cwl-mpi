718ff6d23956facb4496ddc5e40c86aee91d8baa
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2026-09-24 10:36:58 +0200

    Fugaku mpich-sr workflow, and updated PJM tests

---
+++ workflows/mpich-sr/output/cwltool/fugaku/none/workflow-base.cmd
@@ -0,0 +1,1 @@
+COMMAND: cwltool --no-container --debug --strict-cpu-limit --strict-memory-limit --enable-ext --mpi-config-file mpi-config-file.yml --preserve-entire-environment --outdir=logs/cwltool/fugaku/none sr-workflow-base-command.cwl --np 2
